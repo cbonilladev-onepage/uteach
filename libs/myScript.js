fd0175d721daf4469b6c94c0fb9403b9e4e54446
cbonilla <christian.bonilla01@gmail.com>
2020-08-01 17:11:06 -0700

    Buncha' potential stuff that won't be used.

--- libs/myScript.js
+++ libs/myScript.js
@@ -805,3 +805,13 @@ function addWebcam() {
   var hide = $(".hide")
   hide.removeClass("hide")
 }
+
+// document.onreadystatechange = function() { 
+//   if (document.readyState !== "complete") { 
+//       document.querySelector("body").style.visibility = "hidden"; 
+//       document.querySelector("#loader").style.visibility = "visible"; 
+//   } else { 
+//       document.querySelector("#loader").style.display = "none"; 
+//       document.querySelector("body").style.visibility = "visible"; 
+//   } 
+// }; 
